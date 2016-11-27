489a4f07efb6cfe3fc97e681d5214bd54c98cb3a
asugar13 <asierugarteche@Asiers-MacBook-Pro.local>
2016-11-26 19:29:42 -0500

    merge should work now

--- project_directory/assets/scripts/adminScript.js
+++ project_directory/assets/scripts/adminScript.js
@@ -1,20 +1,40 @@
 var admin = {};
 
 
+function deleteButtonOnClick(){
+  $(".deleteUser").click(function(){
+   var button = this;
+   var user = $(button).parents("tr:first").children("td#Username").html();
+   $.ajax({
+     url: '/admin',
+     type: 'DELETE',
+     data: {username: user},
+     success: function(result) {
+       var placeholder = $(button).parents("tr:first");
+       console.log(placeholder);
+       $(button).parents("tr:first").remove();
+     }
+   });
+ });
+}
+
 admin.init = function() {
   $("#displaydiv").empty();
   $("#AddUserForm").hide();
   console.log('this is working');
   $.get('/database', function(data){
+    console.log(data[0].username);
+    console.log('\n');
     for (var i=0; i < data.length; i++) {
       var nested_user = $("<tr></tr>");
-      nested_user.append("<td>" + data[i].username + "</td>");
+      nested_user.append("<td id=Username>" + data[i].username + "</td>");
       nested_user.append("<td>" + data[i].password + "</td>");
       nested_user.append("<td>" + data[i].score + "</td>");
-      nested_user.append("<td> <button onClick='editHandler(this)'> Edit</button> <button>Delete</button> </td>");
-      $("table").append(nested_user);
+      nested_user.append("<td> <button> Edit</button> <button class=deleteUser>Delete</button> </td>");
 
+      $("table").append(nested_user);
     }
+    deleteButtonOnClick();
   });
 }
 

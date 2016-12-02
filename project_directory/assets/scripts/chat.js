18239f689c8a102f2936cd0454a53025350e83ef
alex chernous <alex.a.chernous@gmail.com>
2016-12-01 19:07:45 -0500

    chat

---
+++ project_directory/assets/scripts/chat.js
@@ -0,0 +1,13 @@
+var socket = io('/chatRoom');
+$(document).ready(function() {
+	$('#messageForm').submit(function(){
+		console.log($('#userMsg').val());
+		socket.emit('chatMessage', $('#userMsg').val());
+		$('#userMsg').val('');
+		return false;
+	});
+
+	socket.on('chatMessage', function(msg){
+	    $('#chatBox').append($('<li>').text(msg));
+	  });
+});
